55380caea3f21151bb64cc5b36184b1c5151fc75
pfedick <patrick@pfp.de>
2022-07-08 17:13:41 +0200

    fixed issues in ComboBox

--- src/widgets/combobox.cpp
+++ src/widgets/combobox.cpp
@@ -4,14 +4,14 @@
 namespace Decker::ui {
 
 ComboBox::ComboBox(int x, int y, int width, int height)
-: ppl7::tk::Widget()
+	: ppl7::tk::Widget()
 {
 	selection=NULL;
-	create(x,y,width,height);
-	ppl7::tk::WindowManager *wm=ppl7::tk::GetWindowManager();
+	create(x, y, width, height);
+	ppl7::tk::WindowManager* wm=ppl7::tk::GetWindowManager();
 	myCurrentIndex=0;
 
-	dropdown_button=new ppl7::tk::Label(width-24,0,20,height);
+	dropdown_button=new ppl7::tk::Label(width - 24, 0, 20, height);
 	dropdown_button->setIcon(wm->ButtonSymbols.getDrawable(4));
 	dropdown_button->setEventHandler(this);
 	this->addChild(dropdown_button);
@@ -25,9 +25,9 @@ ComboBox::~ComboBox()
 }
 
 
-void ComboBox::setCurrentText(const ppl7::String &text)
+void ComboBox::setCurrentText(const ppl7::String& text)
 {
-	if (text!=myCurrentText) {
+	if (text != myCurrentText) {
 		myCurrentText=text;
 		needsRedraw();
 	}
@@ -45,10 +45,10 @@ ppl7::String ComboBox::currentIdentifier() const
 
 void ComboBox::setCurrentIndex(size_t index)
 {
-	if (index>=items.size()) return;
+	if (index >= items.size()) return;
 	std::list<ComboBoxItem>::iterator it;
-	for (it=items.begin();it!=items.end();++it) {
-		if ((*it).index==index) {
+	for (it=items.begin();it != items.end();++it) {
+		if ((*it).index == index) {
 			myCurrentIndex=index;
 			myCurrentText=(*it).text;
 			myCurrentIdentifier=(*it).identifier;
@@ -57,11 +57,11 @@ void ComboBox::setCurrentIndex(size_t index)
 	}
 }
 
-void ComboBox::setCurrentIdentifier(const ppl7::String &identifier)
+void ComboBox::setCurrentIdentifier(const ppl7::String& identifier)
 {
 	std::list<ComboBoxItem>::iterator it;
-	for (it=items.begin();it!=items.end();++it) {
-		if ((*it).identifier==identifier) {
+	for (it=items.begin();it != items.end();++it) {
+		if ((*it).identifier == identifier) {
 			myCurrentIndex=(*it).index;
 			myCurrentText=(*it).text;
 			myCurrentIdentifier=(*it).identifier;
@@ -75,14 +75,14 @@ size_t ComboBox::currentIndex() const
 	return myCurrentIndex;
 }
 
-void ComboBox::add(const ppl7::String &text, const ppl7::String &identifier)
+void ComboBox::add(const ppl7::String& text, const ppl7::String& identifier)
 {
 	ComboBoxItem item;
 	item.text=text;
 	item.identifier=identifier;
 	item.index=items.size();
 	items.push_back(item);
-	if (items.size()==1) setCurrentIndex(0);
+	if (items.size() == 1) setCurrentIndex(0);
 	needsRedraw();
 }
 
@@ -91,6 +91,10 @@ void ComboBox::clear()
 {
 	myCurrentText.clear();
 	myCurrentIdentifier.clear();
+	items.clear();
+	if (selection) delete selection;
+	selection=NULL;
+
 	needsRedraw();
 }
 
@@ -99,70 +103,76 @@ ppl7::String ComboBox::widgetType() const
 	return "ComboBox";
 }
 
-void ComboBox::paint(ppl7::grafix::Drawable &draw)
+void ComboBox::paint(ppl7::grafix::Drawable& draw)
 {
-	const ppl7::tk::WidgetStyle &style=ppl7::tk::GetWidgetStyle();
-	int w=width()-1;
-	int h=height()-1;
+	const ppl7::tk::WidgetStyle& style=ppl7::tk::GetWidgetStyle();
+	int w=width() - 1;
+	int h=height() - 1;
 	draw.cls(style.buttonBackgroundColor);
-	draw.drawRect(0,0,w,h,style.frameBorderColorLight);
+	draw.drawRect(0, 0, w, h, style.frameBorderColorLight);
 	ppl7::grafix::Font myFont=style.buttonFont;
 	myFont.setColor(style.labelFontColor);
 	myFont.setOrientation(ppl7::grafix::Font::TOP);
 	ppl7::grafix::Size s=myFont.measure(myCurrentText);
-	draw.print(myFont,4,(draw.height()-s.height)>>1,myCurrentText);
+	draw.print(myFont, 4, (draw.height() - s.height) >> 1, myCurrentText);
 
 
 }
 
-void ComboBox::mouseWheelEvent(ppl7::tk::MouseEvent *event)
+void ComboBox::mouseWheelEvent(ppl7::tk::MouseEvent* event)
 {
 	//printf ("Wheel: %d\n", event->wheel.y);
-	if (event->wheel.y<0 && myCurrentIndex<items.size()-1) {
-		setCurrentIndex(myCurrentIndex+1);
+	if (event->wheel.y < 0 && myCurrentIndex < items.size() - 1) {
+		setCurrentIndex(myCurrentIndex + 1);
 		ppl7::tk::Event ev(ppl7::tk::Event::ValueChanged);
 		ev.setWidget(this);
 		valueChangedEvent(&ev, myCurrentIndex);
 
-	} else if (event->wheel.y>0 && myCurrentIndex>0) {
-		setCurrentIndex(myCurrentIndex-1);
+	} else if (event->wheel.y > 0 && myCurrentIndex > 0) {
+		setCurrentIndex(myCurrentIndex - 1);
 		ppl7::tk::Event ev(ppl7::tk::Event::ValueChanged);
 		ev.setWidget(this);
 		valueChangedEvent(&ev, myCurrentIndex);
 	}
 }
 
-void ComboBox::mouseDownEvent(ppl7::tk::MouseEvent *event)
+void ComboBox::mouseDownEvent(ppl7::tk::MouseEvent* event)
 {
-	if (selection) {
-		delete(selection);
-		selection=NULL;
-	} else {
-		// we need the absolute coordinates of this widget on window
-		// and create a new Frame with window as parent and topmost
-		ppl7::grafix::Point p=absolutePosition();
-		ppl7::tk::Widget *window=getTopmostParent();
-		size_t maxsize=items.size();
-		if (maxsize>10) maxsize=10;
-		selection=new ListWidget(p.x, p.y+this->height(),this->width(),maxsize*30);
-		selection->setTopmost(true);
-		selection->setEventHandler(this);
-		std::list<ComboBoxItem>::const_iterator it;
-		for (it=items.begin();it!=items.end();++it) {
-			selection->add((*it).text, (*it).identifier);
+	if (event->widget() == this || event->widget() == dropdown_button) {
+		//printf("ComboBox::mouseDownEvent\n");
+		if (selection) {
+			delete(selection);
+			selection=NULL;
+		} else {
+			// we need the absolute coordinates of this widget on window
+			// and create a new Frame with window as parent and topmost
+			ppl7::grafix::Point p=absolutePosition();
+			ppl7::tk::Widget* window=getTopmostParent();
+			size_t maxsize=items.size();
+			if (maxsize > 10) maxsize=10;
+			selection=new ListWidget(p.x, p.y + this->height(), this->width(), maxsize * 30);
+			selection->setTopmost(true);
+			selection->setEventHandler(this);
+			std::list<ComboBoxItem>::const_iterator it;
+			for (it=items.begin();it != items.end();++it) {
+				selection->add((*it).text, (*it).identifier);
+			}
+			selection->setCurrentIndex(myCurrentIndex);
+			window->addChild(selection);
 		}
-		selection->setCurrentIndex(myCurrentIndex);
-		window->addChild(selection);
+	} else {
+		EventHandler::mouseDownEvent(event);
 	}
 }
 
-void ComboBox::valueChangedEvent(ppl7::tk::Event *event, int value)
+void ComboBox::valueChangedEvent(ppl7::tk::Event* event, int value)
 {
-	//printf ("ComboBox::valueChangedEvent\n");
-	if (selection!=NULL && event->widget()==selection) {
+	//printf("ComboBox::valueChangedEvent\n");
+	if (selection != NULL && event->widget() == selection) {
 		setCurrentIndex(selection->currentIndex());
 		selection->deleteLater();
 		selection=NULL;
+		needsRedraw();
 		ppl7::tk::Event ev(ppl7::tk::Event::ValueChanged);
 		ev.setWidget(this);
 		valueChangedEvent(&ev, myCurrentIndex);
@@ -174,4 +184,3 @@ void ComboBox::valueChangedEvent(ppl7::tk::Event *event, int value)
 
 
 } //EOF namespace Decker::ui
-
