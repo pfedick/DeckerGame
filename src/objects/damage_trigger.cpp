b6a07c8ee6d74fcad8d45c33d93954565d889ad3
Patrick Fedick <patrick@pfp.de>
2024-08-07 21:59:35 +0200

    created icon for DamageTrigger

--- src/objects/damage_trigger.cpp
+++ src/objects/damage_trigger.cpp
@@ -9,22 +9,22 @@ namespace Decker::Objects {
 
 Representation DamageTrigger::representation()
 {
-	return Representation(Spriteset::GenericObjects, 335);
+    return Representation(Spriteset::GenericObjects, 529);
 }
 
 DamageTrigger::DamageTrigger()
-	: Object(Type::ObjectType::DamageTrigger)
+    : Object(Type::ObjectType::DamageTrigger)
 {
-	sprite_set=Spriteset::GenericObjects;
-	collisionDetection=true;
-	pixelExactCollision=false;
-	sprite_no_representation=335;
-	sprite_no=sprite_no_representation;
-	visibleAtPlaytime=false;
-	range.setPoint(TILE_WIDTH * 6, TILE_HEIGHT * 6);
-	damage_per_second_or_trigger=50;
-	damage_type=Player::HealthDropReason::Unknown;
-	initial_state=true;
+    sprite_set=Spriteset::GenericObjects;
+    collisionDetection=true;
+    pixelExactCollision=false;
+    sprite_no_representation=529;
+    sprite_no=sprite_no_representation;
+    visibleAtPlaytime=false;
+    range.setPoint(TILE_WIDTH * 6, TILE_HEIGHT * 6);
+    damage_per_second_or_trigger=50;
+    damage_type=Player::HealthDropReason::Unknown;
+    initial_state=true;
     triggered_by_collision=true;
 }
 
@@ -63,8 +63,8 @@ size_t DamageTrigger::load(const unsigned char* buffer, size_t size)
     if (version < 1 || version>1) return 0;
 
     int flags=ppl7::Peek8(buffer + bytes + 1);
-    initial_state=(bool) (flags & 1);
-    triggered_by_collision=(bool) (flags & 1);
+    initial_state=(bool)(flags & 1);
+    triggered_by_collision=(bool)(flags & 1);
     range.x=ppl7::Peek16(buffer + bytes + 2);
     range.y=ppl7::Peek16(buffer + bytes + 4);
     damage_per_second_or_trigger=ppl7::Peek8(buffer + bytes + 6);
@@ -81,8 +81,8 @@ void DamageTrigger::update(double time, TileTypePlane&, Player&, float)
 void DamageTrigger::handleCollision(Player* player, const Collision& col)
 {
     if (!enabled) return;
-    if (triggered_by_collision) {
-        player->dropHealth(((float)damage_per_second_or_trigger)/60.0f*col.frame_rate_compensation,
+    if (triggered_by_collision == true && damage_per_second_or_trigger > 0) {
+        player->dropHealth(((float)damage_per_second_or_trigger) / 60.0f * col.frame_rate_compensation,
             static_cast<Physic::HealthDropReason>(damage_type));
     }
 }
@@ -114,8 +114,8 @@ void DamageTrigger::trigger(Object* source)
 {
     if (!enabled) return;
     if (triggered_by_collision) return;
-    GetGame().getPlayer()->dropHealth((float)damage_per_second_or_trigger,
-            static_cast<Physic::HealthDropReason>(damage_type));
+    if (damage_per_second_or_trigger > 0) GetGame().getPlayer()->dropHealth((float)damage_per_second_or_trigger,
+        static_cast<Physic::HealthDropReason>(damage_type));
 }
 
 void DamageTrigger::toggle(bool enable, Object* source)
@@ -132,7 +132,7 @@ private:
     ppltk::CheckBox* initialStateEnabled, * currentState;
     ppltk::RadioButton* triggered_by_collision;
     ppltk::RadioButton* triggered_by_trigger;
-    ppltk::ComboBox *damage_type;
+    ppltk::ComboBox* damage_type;
     DamageTrigger* object;
 
 public:
@@ -168,32 +168,32 @@ DamageTriggerDialog::DamageTriggerDialog(DamageTrigger* object)
     currentState->setEventHandler(this);
     addChild(currentState);
     y+=35;
-    
-    triggered_by_collision=new ppltk::RadioButton(0, y, sw, 30, "triggered by collision", (bool)(object->triggered_by_collision==true));
+
+    triggered_by_collision=new ppltk::RadioButton(0, y, sw, 30, "triggered by collision", (bool)(object->triggered_by_collision == true));
     triggered_by_collision->setEventHandler(this);
     addChild(triggered_by_collision);
-    triggered_by_trigger=new ppltk::RadioButton(sw, y, sw, 30, "triggered by trigger", (bool)(object->triggered_by_collision==false));
+    triggered_by_trigger=new ppltk::RadioButton(sw, y, sw, 30, "triggered by trigger", (bool)(object->triggered_by_collision == false));
     triggered_by_trigger->setEventHandler(this);
     addChild(triggered_by_trigger);
     y+=35;
 
-	addChild(new ppltk::Label(0, y, 120, 30, "Damage Type: "));
-	damage_type=new ppltk::ComboBox(120, y, sw-120, 30);
-	damage_type->add("Misc", ppl7::ToString("%d", Physic::HealthDropReason::Unknown));
+    addChild(new ppltk::Label(0, y, 120, 30, "Damage Type: "));
+    damage_type=new ppltk::ComboBox(120, y, sw - 120, 30);
+    damage_type->add("Misc", ppl7::ToString("%d", Physic::HealthDropReason::Unknown));
     damage_type->add("Falling Deep", ppl7::ToString("%d", Physic::HealthDropReason::FallingDeep));
     damage_type->add("Smashed", ppl7::ToString("%d", Physic::HealthDropReason::Smashed));
     damage_type->add("Drowned", ppl7::ToString("%d", Physic::HealthDropReason::Drowned));
     damage_type->add("Burned", ppl7::ToString("%d", Physic::HealthDropReason::Burned));
     damage_type->add("Etched", ppl7::ToString("%d", Physic::HealthDropReason::Etched));
     damage_type->add("Smashed Sideways", ppl7::ToString("%d", Physic::HealthDropReason::SmashedSideways));
-	damage_type->sortItems();
-	damage_type->setCurrentIdentifier(ppl7::ToString("%d", object->damage_type));
+    damage_type->sortItems();
+    damage_type->setCurrentIdentifier(ppl7::ToString("%d", object->damage_type));
     damage_type->setEventHandler(this);
-	addChild(damage_type);
+    addChild(damage_type);
 
 
     addChild(new ppltk::Label(sw, y, 120, 30, "Damage:"));
-    damage_per_second_or_trigger=new ppltk::HorizontalSlider(sw+120, y, sw - 120, 30);
+    damage_per_second_or_trigger=new ppltk::HorizontalSlider(sw + 120, y, sw - 120, 30);
     damage_per_second_or_trigger->setLimits(1, 255);
     damage_per_second_or_trigger->setValue(object->damage_per_second_or_trigger);
     damage_per_second_or_trigger->enableSpinBox(true, 1, 80);
@@ -224,9 +224,9 @@ DamageTriggerDialog::DamageTriggerDialog(DamageTrigger* object)
 
 void DamageTriggerDialog::valueChangedEvent(ppltk::Event* event, int value)
 {
-if (event->widget() == damage_type) {
-		object->damage_type=damage_type->currentIdentifier().toInt();
-	}
+    if (event->widget() == damage_type) {
+        object->damage_type=damage_type->currentIdentifier().toInt();
+    }
 }
 
 void DamageTriggerDialog::valueChangedEvent(ppltk::Event* event, int64_t value)
@@ -247,14 +247,14 @@ void DamageTriggerDialog::valueChangedEvent(ppltk::Event* event, double value)
 
 void DamageTriggerDialog::toggledEvent(ppltk::Event* event, bool checked)
 {
-    if (event->widget() == triggered_by_collision && checked==true) {
+    if (event->widget() == triggered_by_collision && checked == true) {
         object->triggered_by_collision=true;
         object->collisionDetection=true;
     } else if (event->widget() == initialStateEnabled) {
         object->initial_state=checked;
     } else if (event->widget() == currentState) {
         object->enabled=checked;
-    }  else if (event->widget() == triggered_by_trigger && checked==true) {
+    } else if (event->widget() == triggered_by_trigger && checked == true) {
         object->triggered_by_collision=false;
         object->collisionDetection=false;
     }
